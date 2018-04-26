98d2565adfb8ce45b09974bdaa7329f98d576dc1
Sairam Simha <sairam.simha@gmail.com>
2018-04-25 23:01:52 -0400

    Updates

--- Python/VE01/basic_ops.py
+++ Python/VE01/basic_ops.py
@@ -13,11 +13,15 @@ rl = importlib.reload
 # Pip Install shortcut
 pipin = lambda pkg: pip._internal.main(['install', pkg, '--upgrade'])
 
+
 def LoadModuleLocal(mod_name, gl=globals(), skip_dun=True, only_funcs=False):
     exec('import ' + mod_name, gl)
-    #print(globals())
+    # print(globals())
     try:
         print(eval(mod_name, gl))
     except:
         print('error')
-    exec('\n'.join([(name + '=' + mod_name + '.'+name if (((name[:2]!='__') or not skip_dun) and ((eval('callable(' + mod_name + '.' + name + ')', gl)) or not only_funcs)) else '' ) for name in eval(mod_name, gl).__dict__]).lstrip()+'\n', gl)
+    exec('\n'.join([(name + '=' + mod_name + '.'+name if (((name[:2] != '__')
+            or not skip_dun) and ((eval('callable(' + mod_name + '.' + name
+            + ')', gl)) or not only_funcs)) else '') for name in
+            eval(mod_name, gl).__dict__]).lstrip()+'\n', gl)
